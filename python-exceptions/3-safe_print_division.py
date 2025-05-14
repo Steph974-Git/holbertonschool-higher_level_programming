1daf16b7df6394d2f3f084baddeb36521b7a22db
Steph974-Git <10466@holbertonstudents.com>
2025-05-14 14:03:18 +0200

    Fix safe_print_division to handle ZeroDivisionError without returning None; improve print formatting

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        pass
     finally:
-        print("Inside result: {} ".format(result))
+        print("Inside result: {}".format(result))
     return result
