b17113ab50e5703128bd4e5f859e938f83f89f7d
Steph974-Git <10466@holbertonstudents.com>
2025-05-14 10:39:07 +0200

    Refactor safe_print_list to enforce integer formatting and add safe_print_integer function; update main files and README

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
+        except (ValueError, TypeError):
+            continue
         except IndexError:
-            pass
+            break
     print()
     return count
