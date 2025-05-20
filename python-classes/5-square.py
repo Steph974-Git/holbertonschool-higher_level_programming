c385b5c8ba080b8acb38a66af36015e94c42748e
Steph974-Git <10466@holbertonstudents.com>
2025-05-20 09:54:36 +0200

    Refactor Square class to include size validation in the setter; add my_print method for visual representation

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,7 +6,7 @@ class Square:
 
     def area(self):
         return self.__size ** 2
-    
+
     @property
     def size(self):
         return self.__size
@@ -14,7 +14,14 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if (value < 0):
+            raise TypeError("size must be aninteger")
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
