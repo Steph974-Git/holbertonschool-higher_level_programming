97c2d81539c94a572562ef3bdeabbcb8bd8e956b
Steph974-Git <10466@holbertonstudents.com>
2025-05-26 14:44:07 +0200

    fix: Correct inheritance check logic in inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -20,4 +20,4 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a subclass of a_class,
               False otherwise.
     """
-    return isinstance(obj, a_class) and not isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
