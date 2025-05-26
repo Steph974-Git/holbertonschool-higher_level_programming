6eb665f74bcd9ecd3e096aea23dff13ff42b5d8b
Steph974-Git <10466@holbertonstudents.com>
2025-05-26 14:54:37 +0200

    fix: Update inherits_from function to use isinstance for accurate inheritance checks; enhance test cases for integer_validator

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -20,4 +20,4 @@ def inherits_from(obj, a_class):
         bool: True if obj is an instance of a subclass of a_class,
               False otherwise.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
