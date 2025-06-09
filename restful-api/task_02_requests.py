b61f98f5505b55d792edfab27034b4d1bf50eda7
Steph974-Git <10466@holbertonstudents.com>
2025-06-09 21:26:17 +0200

    feat: Implement functions to fetch posts and save them to CSV

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,8 +12,36 @@ def fetch_and_print_posts():
         data = r.json()
 
         for post in data:
-            print(post['title'])
+            print("{}".format(post['title']))
 
 def fetch_and_save_posts():
 
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
     
+    if r.status_code == 200:
+        data= r.json()
+
+        structured_posts = []
+        for post in data:
+            structured_posts.append({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            })
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+
+            writer.writerows(structured_posts)
+
+    print("Posts saved to posts.csv successfully")
+        
+if __name__ == "__main__":
+    print("Fetching and printing posts:")
+    fetch_and_print_posts()
+            
+    print("\nFetching and saving posts to CSV:")
+    fetch_and_save_posts()
