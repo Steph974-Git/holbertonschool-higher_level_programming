a8ae701c4d21fa17d05d15c7ed8352da8587bf92
Steph974-Git <10466@holbertonstudents.com>
2025-06-23 15:12:19 +0200

    fix: Standardize SQL keywords to uppercase for consistency

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Prints the following description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server
-show create table first_table;
+SHOW create table first_table;
--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Creates a table second_table in the database
 -- hbtn_0c_0 in your MySQL server and add multiples rows
-CREATE TABLE if not exists second_table(
+CREATE TABLE if NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 )
-Insert into second_table(id, name, score) values
+INSERT INTO second_table(id, name, score) values
 
 (1, 'John', 10),
 (2, 'Alex', 3),
