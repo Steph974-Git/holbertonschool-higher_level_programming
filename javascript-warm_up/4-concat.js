748692122576de72135971ed309c78d0c88b46c6
Steph974-Git <10466@holbertonstudents.com>
2025-07-09 15:48:32 +0200

    feat: Add initial JavaScript files for various programming exercises

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+const { argv } = require('node:process');
+const args = argv.slice(2);
+const a = (process.argv[2]);
+const b = (process.argv[3]);
+console.log(a + " is " + b);
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+const { argv } = require('node:process');
+const args = argv.slice(2);
+const a = parseInt(args[0]);
+if (isNaN(a)) {
+    console.log("Not a number");
+} else {
+    console.log("My number: " + a)
+}
